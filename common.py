0b11974ff9b1b95887b50a12ede5de59ab6f615e
Ilya Shabelnikov <ilyuha.bratan@gmail.com>
2022-11-30 18:36:08 +0300

    Add file

---
+++ common.py
@@ -0,0 +1,7 @@
+class Common():
+    def __init__(self, fd = 80000, fs = 21000, ti = 0.1, Tc = 0, d = 0.02):
+        self.fd = fd
+        self.fs = fs
+        self.ti = ti
+        self.Tc = Tc
+        self.d = d
