6d11bbe39bc0f213ffce4f515fb0732e9b982e97
Ilya Shabelnikov <ilyuha.bratan@gmail.com>
2022-12-09 12:47:27 +0300

    Add file

---
+++ gui.py
@@ -0,0 +1,15 @@
+import PySimpleGUI as sg
+import matplotlib.pyplot as plt
+
+layout = [
+    [sg.Text('fd '), sg.InputText()],
+    [sg.Text('fs '), sg.InputText()],
+    [sg.Text('ti  '), sg.InputText()],
+    [sg.Text('Tc'), sg.InputText()],
+    [sg.Text('d  '), sg.InputText()],
+    [sg.Output(size = (100, 20))],
+]
+
+window = sg.Window('File Compare', layout)
+window.read()
+
